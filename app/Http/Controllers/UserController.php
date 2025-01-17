80bcdbd556c1ceeea29c63dde93f586da3fa1713
Rafael A. Ortega <raortega8906@gmail.com>
2025-01-18 00:00:56 +0100

    Fix route in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
     public function index()
     {
         $users = User::all();
-        return view('users.index', compact('users'));
+        return view('admin.users.index', compact('users'));
     }
 
     /**
