d84d2e5f48aa7d28a671ba0d9d9a8895dc26a982
Rafael A. Ortega <raortega8906@gmail.com>
2025-01-18 00:36:00 +0100

    Add logic to ValidateRequest

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -22,7 +22,8 @@ class UpdateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,'.$this->route('user')->id,
         ];
     }
 }
